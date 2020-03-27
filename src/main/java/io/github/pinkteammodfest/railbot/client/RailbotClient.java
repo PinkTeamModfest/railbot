54eb1d1ebc1af3d3efd9e364d14ebb5b1d4891f9
liach <liach@users.noreply.github.com>
2020-03-27 14:55:14 -0500

    Add client entrypoint

---
+++ src/main/java/io/github/pinkteammodfest/railbot/client/RailbotClient.java
@@ -0,0 +1,11 @@
+package io.github.pinkteammodfest.railbot.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public enum RailbotClient implements ClientModInitializer {
+  INSTANCE;
+
+  @Override
+  public void onInitializeClient() {
+  }
+}
