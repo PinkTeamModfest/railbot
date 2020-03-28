582468c769eec472e2f5576f6370550613771aea
liach <liach@users.noreply.github.com>
2020-03-27 21:06:51 -0500

    type as factories
    Signed-off-by: liach <liach@users.noreply.github.com>

--- src/main/java/io/github/pinkteammodfest/railbot/robot/RobotCoreType.java
+++ src/main/java/io/github/pinkteammodfest/railbot/robot/RobotCoreType.java
@@ -1,5 +1,16 @@
 package io.github.pinkteammodfest.railbot.robot;
 
+import java.util.function.Function;
+
 public class RobotCoreType<T extends RobotCore> {
 
+  protected final Function<RobotCoreType<T>, T> factory;
+
+  public RobotCoreType(Function<RobotCoreType<T>, T> factory) {
+    this.factory = factory;
+  }
+
+  public T create() {
+    return factory.apply(this);
+  }
 }
--- src/main/java/io/github/pinkteammodfest/railbot/robot/RobotFeatureType.java
+++ src/main/java/io/github/pinkteammodfest/railbot/robot/RobotFeatureType.java
@@ -1,5 +1,16 @@
 package io.github.pinkteammodfest.railbot.robot;
 
+import java.util.function.Function;
+
 public class RobotFeatureType<T extends RobotFeature> {
 
+  protected final Function<RobotFeatureType<T>, T> factory;
+
+  public RobotFeatureType(Function<RobotFeatureType<T>, T> factory) {
+    this.factory = factory;
+  }
+
+  public T create() {
+    return factory.apply(this);
+  }
 }
