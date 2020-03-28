e5946a3b7e9b973a7c1a88b1f701a4c1836b5118
liach <liach@users.noreply.github.com>
2020-03-27 20:00:03 -0500

    Make property static
    Signed-off-by: liach <liach@users.noreply.github.com>

--- src/main/java/io/github/pinkteammodfest/railbot/block/RailBlock.java
+++ src/main/java/io/github/pinkteammodfest/railbot/block/RailBlock.java
@@ -14,26 +14,34 @@ import net.minecraft.util.math.Direction;
 
 public class RailBlock extends Block {
 
-  private final Map<Direction, BooleanProperty> connections;
+  private static final Map<Direction, BooleanProperty> CONNECTIONS;
+
+  static {
+    CONNECTIONS = new EnumMap<>(Direction.class);
+    for (Direction each : Direction.values()) {
+      CONNECTIONS.put(each, BooleanProperty.of(each.name()));
+    }
+  }
+
+  public static BooleanProperty getConnectionProperty(Direction direction) {
+    return CONNECTIONS.get(direction);
+  }
 
   public RailBlock(Settings settings) {
     super(settings);
-    this.connections = new EnumMap<>(Direction.class);
-    for (Direction each : Direction.values()) {
-      this.connections.put(each, BooleanProperty.of(each.name()));
-    }
   }
 
   @Override
   protected void appendProperties(Builder<Block, BlockState> builder) {
     super.appendProperties(builder);
-    builder.add(this.connections.values().toArray(new Property<?>[0]));
+    builder.add(RailBlock.CONNECTIONS.values().toArray(new Property<?>[0]));
   }
 
   @Override
   public void addStacksForDisplay(ItemGroup group, DefaultedList<ItemStack> list) {
-    if (group != ItemGroup.SEARCH || group != ItemGroup.MISC)
+    if (group != ItemGroup.SEARCH || group != ItemGroup.MISC) {
       return; // add to misc for now
+    }
     super.addStacksForDisplay(group, list);
   }
 }
